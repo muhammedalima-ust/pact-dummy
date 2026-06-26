b1c0dcfb4d94161e66ca489895eef7cc70daaee6
Muhammed Ali M A <310919@ust.com>
2026-06-26 10:19:42 +0530

    cheanged to the wiremock extention

--- citesting/src/test/java/com/citesting/contract/oms/OmsProviderVerification.java
+++ citesting/src/test/java/com/citesting/contract/oms/OmsProviderVerification.java
@@ -11,13 +11,18 @@ import au.com.dius.pact.provider.junitsupport.loader.SelectorBuilder;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 @Provider("oms-provider")
 @PactBroker(
@@ -27,19 +32,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
     includeWipPactsSince = "2026-06-26"
 )
 public class OmsProviderVerification {
-
-    private static final WireMockServer wireMock =
-        new WireMockServer(WireMockConfiguration.options().port(4010));
-
-    @BeforeAll
-    static void startWireMock() {
-        wireMock.start();
-    }
-
-    @AfterAll
-    static void stopWireMock() {
-        wireMock.stop();
-    }
+    @RegisterExtension
+    private static final WireMockExtension wireMock =
+        WireMockExtension.newInstance()
+            .options(wireMockConfig().port(4010))
+            .build();
 
 
     @PactBrokerConsumerVersionSelectors
@@ -48,8 +45,7 @@ public class OmsProviderVerification {
     }
 
     @BeforeEach
-    void setup(PactVerificationContext context) {
-        wireMock.resetAll();                                        
+    void setup(PactVerificationContext context) {                                     
         context.setTarget(new HttpTestTarget("127.0.0.1", 4010));
     }
 
